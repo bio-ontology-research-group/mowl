e102d3e9f58e7920006c98b9a404f787a8230689
Maxat Kulmanov <coolmaksat@gmail.com>
2020-11-10 07:13:06 +0300

    initial model structure

---
+++ mowl/model.py
@@ -0,0 +1,13 @@
+class Model(object):
+
+    def __init__(self, *args, **kwargs):
+        # TODO: initialize parameters
+        pass
+
+    def train(self):
+        # TODO: start training the model here
+        pass
+
+    def evaluate(self):
+        # TODO: evaluate the model here
+        pass
