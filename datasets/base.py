616b87a46ea9f463ee8dfa5ba794471b495de7ef
Maxat Kulmanov <coolmaksat@gmail.com>
2021-01-29 18:09:12 +0300

    .

--- datasets/base.py
+++ datasets/base.py
@@ -1,48 +1,52 @@
+from dataclasses import dataclass
 import numpy as np
-from owlready2 import get_ontology
+from typing import Mapping
 
 
+@dataclass
+class Triples(object):
+
+    filepath: str
+    _loaded: False
+
+    def load(self, delimiter='\t', encoding=None):
+        if not self._loaded:
+            self._triples = np.loadtxt(
+                fname=self.filepath,
+                dtype=str,
+                comments='@Comment@ Head Relation Tail',
+                delimiter=delimiter,
+                encoding=encoding,
+            )
+            self._loaded = True
+        return self._triples
+
+    
+
+@dataclass
 class Dataset(object):
-    def __init__(self, url, split_ratio = (0.6,0.2,0.2), *args, **kwargs):
-        self.url = url
-        self._loaded = False
-        self.split_ratio = split_ratio
-
-    def _load(self):
-        raise NotImplementedError()
-
 
     @property
-    def train_set(self):
+    def training(self):
         if not self._loaded:
             self._load()
         return self._training
-    
-    
+        
     @property
-    def test_set(self):
+    def testing(self):
         if not self._loaded:
             self._load()
         return self._testing
     
     @property
-    def valid_set(self):
+    def validation(self):
         if not self._loaded:
             self._load()
         return self._validation
     
-
-    @classmethod
-    def owl2ntriples(source_file, target_file):
-        onto = get_ontology(source_file).load()
-        onto.save(file = target_file, format = 'ntriples')
-
     @classmethod
     def split_data(data, ratio):
         raise NotImplementedError()
 
     def negative_samples(self):
         raise NotImplementedError()
-
-
-#TODO: Classes for handling remote (tar/zip) and local files
