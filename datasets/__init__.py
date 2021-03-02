e4be497e1c2dcba25db689a13ea1f08a4a77b253
Maxat Kulmanov <coolmaksat@gmail.com>
2021-03-02 13:24:48 +0300

    start jvm in init datasets

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,3 +1,16 @@
-from py4j.java_gateway import JavaGateway
+import jpype
+import jpype.imports
+import os
 
-java_gateway = JavaGateway()
+jars_dir = "gateway/build/distributions/gateway/lib/"
+jars = f'{str.join(":", [jars_dir + name for name in os.listdir(jars_dir)])}'
+
+if not jpype.isJVMStarted():
+    jpype.startJVM(
+        jpype.getDefaultJVMPath(), "-ea",
+        "-Djava.class.path=" + jars,
+        convertStrings=False)
+
+
+from .ppi_yeast import PPIYeastDataset
+
