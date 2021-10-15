587f420c62e538b0b724e145c9f8511be9594b29
Fernando Zhapa <ferynando7@gmail.com>
2021-10-15 18:13:54 +0300

    losses

---
+++ mowl/catEmbeddings/model.py
@@ -0,0 +1,190 @@
+import click as ck
+import torch as th
+import torch.nn as nn
+from torch import optim
+from torch.utils.data import IterableDataset, DataLoader
+import numpy as np
+from sklearn.model_selection import train_test_split
+import random
+from math import floor
+import logging
+
+
+from mowl.model import Model
+from mowl.graph.taxonomy.model import TaxonomyParser
+
+logging.basicConfig(level=logging.DEBUG)
+
+class CatEmbeddings(Model):
+    def __init__(self, dataset, file_params = None, seed = 0):
+        super().__init__(dataset)
+        self.file_params = file_params
+
+        if seed>=0:
+            th.manual_seed(seed)
+            np.random.seed(seed)
+            random.seed(seed)
+
+    
+
+    def train(self):
+
+        #Loading subclass axioms
+        parser = TaxonomyParser(self.dataset, subclass=True, relations=False)
+        edges = parser.parseOWL()
+        logging.info("Parsing ontology finished")
+
+        objects = set()
+
+        for edge in edges:
+            src = str(edge.src())
+            dst = str(edge.dst())
+
+            objects |= {("", "", src), ("", "", dst), ("up", src, dst), ("down", src, dst), ("exp", src, dst)}
+
+        objects_idx = {obj: i for i, obj in enumerate(objects)}
+
+        #Sanity check
+        srcs = [str(e.src()) for e in edges]
+        dsts = [str(e.dst()) for e in edges]
+        classes = list(set(srcs).union(set(dsts)))
+
+
+        batch_size = 64
+        logging.info("Number of objects: %d %d", len(objects), len(classes)+ 3*len(edges))
+
+        train_set, val_set = train_test_split(edges, train_size = 0.8, shuffle = True)
+        logging.debug("Data splitting done")
+
+        train_set = CatDataset(train_set, objects_idx)
+        logging.debug("Train dataset created")
+        val_set = CatDataset(val_set, objects_idx)
+        logging.debug("Val dataset created")
+        train_data_loader = DataLoader(train_set, batch_size = batch_size)
+        logging.debug("Train dataloader created")
+        val_data_loader = DataLoader(val_set, batch_size = batch_size)
+        logging.debug("Val dataloader created")
+
+        
+        model = CatModel(len(classes), len(edges), 2048)
+        paramss = sum(p.numel() for p in model.parameters())
+        trainParamss = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        print(paramss, trainParamss)
+        logging.debug("Model created")
+        optimizer = optim.Adam(model.parameters(), lr = 0.000001)
+
+
+        criterion = lambda x: th.sum(x)
+        for epoch in range(128):
+
+            epoch_loss = 0
+
+            model.train()
+
+            with ck.progressbar(train_data_loader) as bar:
+                for i, (batch_objects, batch_morphisms) in enumerate(bar):
+                    res = model(batch_objects, batch_morphisms)
+                    loss = criterion(res)
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                    epoch_loss += loss.detach().item()
+
+                epoch_loss /= (i+1)
+            
+            print(f'Epoch {epoch}: Loss - {epoch_loss}') #, \tVal loss - {val_loss}, \tAUC - {roc_auc}')
+
+        
+            
+class CatModel(nn.Module):
+
+    def __init__(self, num_objects, num_axioms, embedding_size):
+        super(CatModel, self).__init__()
+
+        num_obj = num_objects + 3*num_axioms
+
+        dim0 = embedding_size
+        dim1 = floor(dim0/2)
+        dim2 = floor(dim1/2)
+        self.net_object = nn.Sequential(
+            nn.Embedding(num_obj, embedding_size),
+            nn.Linear(dim0, dim1),
+            nn.Linear(dim1, dim2)
+        )
+
+        self.net_morphism = nn.Sequential(
+            nn.Embedding(7, embedding_size),
+            nn.Linear(dim0, dim1),
+            nn.Linear(dim1, dim2)
+        )
+        
+    def compute_loss(self, objects, morphisms):
+
+        objects = map(lambda x: x.unsqueeze(1), objects)
+        prod_up, prod_down, exponential, antecedent, consequent = map(self.net_object, objects)
+
+        morphisms = map(lambda x: x.unsqueeze(1), morphisms)
+        proj_up_exp, proj_up_ant, proj_down_exp, proj_down_ant, prod_up_down, prod_up_cons, prod_down_cons = map(self.net_morphism, morphisms)
+
+
+        loss1 = abs(exponential - prod_up - proj_up_exp)
+
+        loss2 = abs(antecedent - prod_up - proj_up_ant)
+        
+        loss3 = abs(exponential - prod_down - proj_down_exp)
+        loss4 = abs(antecedent - prod_down - proj_down_ant)
+
+        loss5 = abs(prod_down - prod_up - prod_up_down)
+        loss6 = abs(consequent - prod_down - prod_down_cons)
+        loss7 = abs(consequent - prod_up - prod_up_cons)
+
+        path_loss1 = abs(proj_up_exp - proj_down_exp - prod_up_down)
+        path_loss2 = abs(proj_up_ant - proj_down_ant - prod_up_down)
+        path_loss3 = abs(prod_up_cons - prod_down_cons - prod_up_down)
+
+        return sum([loss1, loss2, loss3, loss4, loss5, loss6, loss7, path_loss1, path_loss2, path_loss3])
+
+        
+        
+    def forward(self, objects, morphisms):      
+        return self.compute_loss(objects, morphisms)
+        
+
+        
+        
+
+
+    
+
+    
+class CatDataset(IterableDataset):
+    def __init__(self, edges, object_dict):
+        self.edges = edges
+        self.object_dict = object_dict
+
+        
+    def get_data(self):
+
+        for edge in self.edges:
+            antecedent = str(edge.src())
+            consequent = str(edge.dst())
+
+            morphisms = tuple(range(7))
+
+            prod_up = self.object_dict[("up", antecedent, consequent)]
+            prod_down = self.object_dict[("down", antecedent, consequent)]
+            exponential = self.object_dict[("exp", antecedent, consequent)]
+            antecedent = self.object_dict[("", "", antecedent)]
+            consequent = self.object_dict[("", "", consequent)]
+
+            objects = (prod_up, prod_down, exponential, antecedent, consequent)
+
+            yield objects, morphisms
+        
+
+    def __iter__(self):
+        return self.get_data()
+
+    def __len__(self):
+        return len(self.edges)
---
+++ mowl/examples/CatEmbeddings/catEmbed.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import click as ck
+import numpy as np
+import pandas as pd
+import pickle
+import gzip
+import os
+import sys
+import logging
+
+logging.basicConfig(level=logging.INFO)   
+sys.path.insert(0, '')
+sys.path.append('../../../')
+
+from mowl.datasets.base  import PathDataset
+from mowl.catEmbeddings.model import CatEmbeddings
+
+@ck.command()
+@ck.option(
+    '--config', '-c', help="Configuration file in config/")
+
+def main(config):
+    logging.info(f"Number of cores detected: {os.cpu_count()}")
+
+
+    ontology = "data/goslim_yeast.owl"
+    
+    ds = PathDataset(ontology, "", "")
+        
+    model = CatEmbeddings(ds)
+
+    model.train()
+    model.evaluate()
+
+
+if __name__ == '__main__':
+    main()
