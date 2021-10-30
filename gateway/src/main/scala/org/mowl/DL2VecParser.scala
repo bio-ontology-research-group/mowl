c22310875ae4a25413ebc121aee839cd31effa60
Fernando Zhapa <ferynando7@gmail.com>
2021-10-30 21:20:23 +0300

    DL2Vec

---
+++ gateway/src/main/scala/org/mowl/DL2VecParser.scala
@@ -0,0 +1,22 @@
+package org.mowl.DL2VecParser
+
+//OWL API imports
+import org.semanticweb.owlapi.model._
+
+// Java imports
+
+
+import collection.JavaConverters._
+
+
+
+class DL2VecParser(var ontology: OWLOntology) {
+
+  private val ont_manager = OWLManager.createOWLOntologyManager()
+  private val data_factory = ont_manager.getOWLDataFactory()
+
+  def parse = {
+    val 
+  }
+
+}
