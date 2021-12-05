5dfa4bd22c2557d6e4d4c9f7fdc5c2bdbe7fa641
Fernando Zhapa <ferynando7@gmail.com>
2021-12-05 16:02:50 +0300

    update

--- mowl/catEmbeddings/model.py
+++ mowl/catEmbeddings/model.py
@@ -16,7 +16,7 @@ import time
 from itertools import chain
 
 from mowl.model import Model
-from mowl.graph.taxonomy.model import TaxonomyParser
+from org.mowl.Parsers import TaxonomyParser
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -37,15 +37,16 @@ class CatEmbeddings(Model):
 
         train_data_loader, val_data_loader, num_classes, num_edges, num_objects = self.load_data()
         
-        model = CatModel(num_classes, num_edges, num_objects, 2048)
+        model = CatModel(num_classes, num_edges, num_objects, 128)
         paramss = sum(p.numel() for p in model.parameters())
         logging.debug("Model created")
-        optimizer = optim.Adam(model.parameters(), lr = 0.000000001, weight_decay=0)
+        optimizer = optim.Adam(model.parameters(), lr = 0.000001, weight_decay=0.01)
 
 
         criterion = lambda x: th.mean(x)
         criterion2 = nn.BCELoss()
-        for epoch in range(50):
+        best_roc_auc = 0
+        for epoch in range(1000):
 
             epoch_loss = 0
 
@@ -60,8 +61,8 @@ class CatEmbeddings(Model):
                     lbls = th.cat([th.ones(batch_size), th.zeros(batch_size)], 0)
                     
                     classif_loss = criterion2(logits.squeeze(), lbls.float())
-                    print(cat_loss.detach(), classif_loss.detach())
-                    loss = cat_loss + classif_loss
+#                    print(cat_loss.detach(), classif_loss.detach())
+                    loss = classif_loss
                     optimizer.zero_grad()
                     loss.backward()
                     optimizer.step()
@@ -75,19 +76,19 @@ class CatEmbeddings(Model):
             val_loss = 0
             preds = []
             labels = []
-            best_roc_auc = 0
+        
             with th.no_grad():
                 optimizer.zero_grad()
                 with ck.progressbar(val_data_loader) as bar:
                     for i, (batch_objects_pos, batch_objects_neg1, batch_objects_neg2) in enumerate(bar):
-                        logits = model(batch_objects_pos, batch_objects_neg1, batch_objects_neg2)
-
+                        cat_loss, logits = model(batch_objects_pos, batch_objects_neg1, batch_objects_neg2)
+                        cat_loss = criterion(cat_loss)
                         batch_size = len(batch_objects_pos[0])
                         lbls = th.cat([th.ones(batch_size), th.zeros(batch_size)], 0)
 
                         labels = np.append(labels, lbls.cpu())
                         preds = np.append(preds, logits.cpu())
-                        loss = criterion2(logits.squeeze(), lbls)
+                        loss = criterion2(logits.squeeze(), lbls)# + cat_loss
                         val_loss += loss.detach().item()
 
                     val_loss /= (i+1)
@@ -95,8 +96,9 @@ class CatEmbeddings(Model):
             roc_auc = compute_roc(labels, preds)
             if roc_auc > best_roc_auc:
                 best_roc_auc = roc_auc
+                best_epoch = epoch
 #                th.save(model.state_dict(), self.file_params["output_model"])
-            print(f'Epoch {epoch}: Loss - {epoch_loss}, \tVal loss - {val_loss}, \tAUC - {roc_auc}')
+            print(f'Epoch {epoch}: Loss - {epoch_loss}, \tVal loss - {val_loss}, \tAUC - {roc_auc}, \tBest({best_epoch}) - {best_roc_auc}')
 #            print(f'Epoch {epoch}: Loss - {epoch_loss}, \tVal AUC - {roc_auc}')
 
  #           print(f'Epoch {epoch}: Loss - {epoch_loss}') #, \tVal loss - {val_loss}, \tAUC - {roc_auc}')
@@ -241,8 +243,8 @@ def generate_negatives(train_set, val_set):
                     train_entities.remove(newD)
             end = time.time()
 
-#        outfile_train = open(neg_train_file, 'wb')
-#        pkl.dump(train_neg, outfile_train)
+        outfile_train = open(neg_train_file, 'wb')
+        pkl.dump(train_neg, outfile_train)
         
         logging.debug("Traning negatives generated")
 
@@ -273,8 +275,8 @@ def generate_negatives(train_set, val_set):
                     val_entities.remove(newD)
             end = time.time()
             
-#        outfile_val = open(neg_val_file, 'wb')
-#        pkl.dump(val_neg, outfile_val)
+        outfile_val = open(neg_val_file, 'wb')
+        pkl.dump(val_neg, outfile_val)
 
         logging.debug("Validation negatives generated")
 
@@ -287,7 +289,7 @@ def generate_negatives(train_set, val_set):
     print("VALLLLL: ", len(srcs_val), len(val_neg))
     return train_neg, val_neg
 
-class CatModel(nn.Module):
+class CatModel2(nn.Module):
 
     def __init__(self, num_classses, num_axioms, num_objects, embedding_size):
         super(CatModel, self).__init__()
@@ -303,14 +305,17 @@ class CatModel(nn.Module):
             nn.Linear(dim1, dim2)
         )
 
-        
-        self.proj_up_exp = nn.Linear(dim2, dim2)
-        self.proj_up_ant = nn.Linear(dim2, dim2)
+        #Diagram 1
         self.proj_down_exp = nn.Linear(dim2, dim2)
         self.proj_down_ant = nn.Linear(dim2, dim2)
-        self.prod_up_down = nn.Linear(dim2, dim2)
-        self.prod_up_cons = nn.Linear(dim2, dim2)
-        self.prod_down_cons = nn.Linear(dim2, dim2)
+
+        #Diagram 2
+        self.inj_exp_cop = nn.Linear(dim2, dim2)
+        self.inj_cons_cop = nn.Linear(dim2, dim2)
+        self.inj_exp_max = nn.Linear(dim2, dim2)
+        self.inj_cons_max = nn.Linear(dim2, dim2)
+        self.coprod_morphism = nn.Linear(dim2, dim2)
+        
         self.sim = nn.Linear(dim2, dim2)
 
         self.classif = nn.Linear(dim2,1)
@@ -318,26 +323,33 @@ class CatModel(nn.Module):
         
     def compute_loss_train(self, objects):
 
-        prod_up, antecedent, consequent = map(self.net_object, objects)
-        exponential = consequent * antecedent
-        prod_down = exponential * antecedent
+        #Diagram 1
+        _, antecedent, consequent = map(self.net_object, objects)
+        initial = th.ones(antecedent.shape)
         
-        full = True
+        exponential = initial
+        maximum = initial
+        prod_down = exponential * antecedent
+
+        #Diagram 2
+        coproduct = initial + consequent
+        
+        full = False
         if full:
-            loss1 = abs(exponential - self.proj_up_exp(prod_up))
-            loss2 = abs(antecedent - self.proj_up_ant(prod_up))
+            #Diagram 1
+            loss1 = abs(exponential - self.proj_down_exp(prod_down))
+            loss2 = abs(antecedent - self.proj_down_ant(prod_down))
 
-            loss3 = abs(exponential - self.proj_down_exp(prod_down))
-            loss4 = abs(antecedent - self.proj_down_ant(prod_down))
+            #Diagram 2
+            loss3 = abs(coproduct - self.inj_exp_cop(exponential) )
+            loss4 = abs(coproduct - self.inj_cons_cop(consequent) )
+            loss5 = abs(maximum - self.inj_exp_max(exponential) )
+            loss6 = abs(maximum - self.inj_cons_max(consequent) )
+            loss7 = abs(maximum - self.coprod_morphism(coproduct) )
 
-            loss5 = abs(prod_down - self.prod_up_down(prod_up))
-            loss6 = abs(consequent - self.prod_down_cons(prod_down))
-            loss7 = abs(consequent - self.prod_up_cons(prod_up))
-
-            path_loss1 = matrix_norm(self.proj_up_exp.weight - self.prod_up_down.weight@self.proj_down_exp.weight)
-            path_loss2 = matrix_norm(self.proj_up_ant.weight - self.prod_up_down.weight@self.proj_down_ant.weight)
-            path_loss3 = matrix_norm(self.prod_up_cons.weight - self.prod_up_down.weight@self.prod_down_cons.weight)
-
+            path_loss1 = matrix_norm(self.inj_exp_max.weight - self.coprod_morphism.weight@self.inj_exp_cop.weight)
+            path_loss2 = matrix_norm(self.inj_cons_max.weight - self.coprod_morphism.weight@self.inj_cons_cop.weight )
+            
         sim_loss  = abs(consequent - self.sim(antecedent))
 
         logit = th.sigmoid(self.classif(sim_loss))
@@ -345,7 +357,7 @@ class CatModel(nn.Module):
         if not full:
             return th.empty(1), logit
         else:
-            return sum([loss1, loss2, loss3, loss4, loss5, loss6, loss7, path_loss1, path_loss2, path_loss3, sim_loss]), logit
+            return sum([loss1, loss2, loss3, loss4, loss5, loss6, loss7, path_loss1, path_loss2, sim_loss]), logit
 
     def compute_loss_test(self, objects):
 
@@ -374,6 +386,119 @@ class CatModel(nn.Module):
             logits = th.cat([loss_pos, loss_neg], 0)
             return logits
 
+        
+        
+class CatModel(nn.Module):
+
+    def __init__(self, num_classses, num_axioms, num_objects, embedding_size):
+        super(CatModel, self).__init__()
+
+        num_obj = num_objects 
+
+        dim0 = embedding_size
+        dim1 = floor(dim0/2)
+        dim2 = floor(dim1/2)
+        self.net_object = nn.Sequential(
+            nn.Embedding(num_obj, embedding_size),
+            nn.Linear(dim0, dim1),
+            nn.Linear(dim1, dim2)
+        )
+
+        self.exp_repr = nn.Sequential(
+            nn.Linear(2*dim2, 1024),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(1024, dim2)
+        )
+        
+        self.prod_up_repr = nn.Sequential(
+            nn.Linear(dim2, 1024),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(1024, dim2)
+        )
+
+        self.prod_down_repr = nn.Sequential(
+            nn.Linear(2*dim2, 1024),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(1024, dim2)
+        )
+
+        self.similarity = nn.Sequential(
+            nn.Linear(5*dim2, 1024),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(1024, 1)
+        )
+
+        self.similarity_simple = nn.Sequential(
+            nn.Linear(2*dim2, 1024),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(1024, 1)
+        )
+
+        self.proj_up_exp = nn.Linear(dim2, dim2)
+        self.proj_up_ant = nn.Linear(dim2, dim2)
+        self.proj_down_exp = nn.Linear(dim2, dim2)
+        self.proj_down_ant = nn.Linear(dim2, dim2)
+        self.prod_up_down = nn.Linear(dim2, dim2)
+        self.prod_up_cons = nn.Linear(dim2, dim2)
+        self.prod_down_cons = nn.Linear(dim2, dim2)
+        self.sim = nn.Linear(dim2, dim2)
+
+        self.classif = nn.Linear(dim2,1)
+        self.dropout = nn.Dropout()
+        
+    def compute_loss(self, objects):
+
+        antecedent, consequent = map(self.net_object, objects)
+
+        prod_up = self.prod_up_repr(antecedent)
+        exponential = self.exp_repr(th.cat([antecedent, consequent], dim = 1))
+        prod_down = self.prod_down_repr(th.cat([exponential, antecedent], dim=1))
+        
+        full = True
+        if not full:
+#            prod_down2 = self.prod_up_down(prod_up)
+#            print("w: ", th.max(self.prod_up_down.weight), th.min(self.prod_up_down.weight) )
+#            cat_loss = th.norm(prod_down-prod_down2)
+ #           loss1 = abs(exponential - self.proj_up_exp(prod_up))
+ #           loss2 = abs(antecedent - self.proj_up_ant(prod_up))
+
+ #           loss3 = abs(exponential - self.proj_down_exp(prod_down))
+ #           loss4 = abs(antecedent - self.proj_down_ant(prod_down))
+
+ #           loss5 = abs(prod_down - self.prod_up_down(prod_up))
+ #           loss6 = abs(consequent - self.prod_down_cons(prod_down))
+ #           loss7 = abs(consequent - self.prod_up_cons(prod_up))
+
+#            path_loss1 = matrix_norm(self.proj_up_exp.weight - self.prod_up_down.weight@self.proj_down_exp.weight)
+#            path_loss2 = matrix_norm(self.proj_up_ant.weight - self.prod_up_down.weight@self.proj_down_ant.weight)
+#            path_loss3 = matrix_norm(self.prod_up_cons.weight - self.prod_up_down.weight@self.prod_down_cons.weight)
+            sim_loss  = self.similarity_simple(th.cat([antecedent, consequent], dim=1))  #abs(consequent - s            
+        else:
+            sim_loss  = self.similarity(th.cat([antecedent, consequent, exponential, prod_up, prod_down], dim=1))  #abs(consequent - self.sim(antecedent))
+
+        logit = th.sigmoid(sim_loss) #th.sigmoid(self.classif(sim_loss))
+
+        if not full:
+            return th.zeros(antecedent.shape[0]), logit
+        else:
+            return th.zeros(antecedent.shape[0]), logit
+
+        
+        
+    def forward(self, positive, negative1, negative2):
+        loss_pos, logit_pos = self.compute_loss(positive)
+        neg = random.choice([negative1, negative2])
+        #            neg = negative1
+        loss_neg, logit_neg = self.compute_loss(neg)
+        logits = th.cat([logit_pos, logit_neg], 0)
+        margin = 0.1
+        cat_loss = F.relu(loss_pos - loss_neg + margin) 
+        return cat_loss, logits
         
         
 
@@ -413,16 +538,8 @@ class CatDataset(IterableDataset):
         antecedent_ = self.object_dict[("", "", antecedent)]
         consequent_ = self.object_dict[("", "", consequent)]
 
-        if self.mode == "train":
-            prod_up = self.object_dict[("up", antecedent, consequent)]
-
-            return prod_up, antecedent_, consequent_
-
-        elif self.mode in ["val", "test"]:
-            return antecedent_, consequent_
-        else:
-            ValueError()
-                           
+        return antecedent_, consequent_
+                            
 def compute_roc(labels, preds):
     # Compute ROC curve and ROC area for each class
     fpr, tpr, _ = roc_curve(labels.flatten(), preds.flatten())
