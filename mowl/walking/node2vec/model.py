bb90eb1b45b6722b2844c0c0ba5022919c8cf513
Fernando Zhapa-Camacho <ferynando7@gmail.com>
2021-12-22 01:11:58 +0300

    :construction: Added Node2Vec as subclass of Walking abstract class. Test missing

--- mowl/walking/node2vec/model.py
+++ mowl/walking/node2vec/model.py
@@ -1,251 +1,231 @@
-from mowl.model import Model
+from mowl.walking.walking import WalkingModel
 import networkx as nx
 import numpy as np
 import random
-from gensim.models import Word2Vec
+import logging
+from multiprocessing import Pool, get_context
 
-class Node2Vec():
-	'''
-	Reference implementation of node2vec. 
+class Node2Vec(WalkingModel):
+    
+    '''
+    Reference implementation of node2vec. 
 
-	:param input: supported input format is an edgelist of format: node1_id_int node2_id_int <weight_float, optional>
-	:type input: edgelist
-	:param p: Return hyperparameter. Default is 1.
-	:type p: float
-	:param q: Inout hyperparameter. Default is 1.
-	:type p: float
-	:param num_walks: Number of walks per source. Default is 10.
-	:type num_walks: int
-	:param walk_length: Length of walk per source. Default is 80.
-	:type walk_length: int
-	:param embeddings_file_path: output file
-	:type embeddings_file_path: str
-	:param dimensions: Number of dimensions in embedding. Default is 128.
-	:type dimensions: int
-	:param window_size: Context size for optimization. Default is 10.
-	:type window_size: int
-	:param workers: Number of parallel workers. Default is 8.
-	:type workers: int
-	:param iter: Number of epochs in Steepest Gradient Descent (SGD). 
-	:type iter: int
-	:param is_directed: Whether Graph is directed. Default is undirected (False).
-	:type is_directed: boolean
-	:param is_weighted: Whether graph is weighted. Defual is unweighted (False)
-	:type is_weighted: boolean
+    :param p: Return hyperparameter. Default is 1.
+    :type p: float
+    :param q: Input hyperparameter. Default is 1.
+    :type p: float
 
-	Original author: Aditya Grover
+    Original author: Aditya Grover
 
-	For more details, refer to the paper:
-	node2vec: Scalable Feature Learning for Networks
-	Aditya Grover and Jure Leskovec 
-	Knowledge Discovery and Data Mining (KDD), 2016
+    For more details, refer to the paper:
+    node2vec: Scalable Feature Learning for Networks
+    Aditya Grover and Jure Leskovec 
+    Knowledge Discovery and Data Mining (KDD), 2016
 
-	Adapted by Sonja Katz, 2021
-	'''
+    Adapted by Sonja Katz, 2021
+    Readapted to subclass WalkingModel abstract class. By F. Zhapa 
+    '''
 
-	def __init__(self, 
-				edgelist, 
-				p, 
-				q, 
-				num_walks, 
-				walk_length, 
-				embeddings_file_path,
-				dimensions=128, 
-				window_size=10, 
-				workers=8, 
-				iter=1, 
-				is_directed=False, 
-				is_weighted=False, 
-				data_root = "."): 
+    def __init__(self, 
+                 edges, 
+		 num_walks, 
+		 walk_length, 
+		 p, 
+		 q,
+                 num_workers=1,
+                 seed = 0,
+		 outfile = None): 
+        
+        super().__init__(edges, num_walks, walk_length, num_workers, outfile) 
 
-		#super().__init__(dataset) 
-		self.data_root = data_root
-		self.edgelist = edgelist
-		self.p = p
-		self.q = q
-		self.num_walks = num_walks
-		self.walk_length = walk_length
-		self.dimensions = dimensions
-		self.window_size = window_size
-		self.workers = workers
-		self.iter = iter
-		self.is_directed = is_directed
-		self.is_weighted = is_weighted
-		self.embeddings_file_path=f"{self.data_root}/{embeddings_file_path}"
-		
-	def read_graph(self):
-		'''
-		Reads the input network in networkx.
+        self.p = p
+        self.q = q
 
-		Returns the network graph
-		'''
+        # self.is_directed = is_directed
+	# self.is_weighted = is_weighted
+	# self.embeddings_file_path=f"{self.data_root}/{embeddings_file_path}"
 
-		if self.is_weighted:
-			G = nx.read_edgelist(self.edgelist, nodetype=int, data=(('weight',float),), create_using=nx.DiGraph())
-		else:
-			G = nx.read_edgelist(self.edgelist, nodetype=int, create_using=nx.DiGraph())
-		for edge in G.edges():
-			G[edge[0]][edge[1]]['weight'] = 1
+        self.graph = nx.Graph()
+        
+        for edge in edges:
+            src, rel, dst, weight = edge.src(), edge.rel(), edge.dst(), edge.weight()
+            self.graph.add_edge(src, dst)
+            self.graph.edges[src,dst]["type"] = rel
+            self.graph.edges[src,dst]["weight"] = weight
 
-		if not self.is_directed:
-			G = G.to_undirected()
+        self.nodes = list(self.graph.nodes())
 
-		self.G = G
 
-		return G
+    def walk(self):
+                
+        paths_per_worker = self.num_paths_per_worker()
 
-	def node2vec_walk(self, walk_length, start_node):
-		'''
-		Simulate a random walk starting from start node.
-		'''
-		G = self.G
-		alias_nodes = self.alias_nodes
-		alias_edges = self.alias_edges
+        self.preprocess_transition_probs(G)
+    
+        file_names = [f"tmpfile_{i}.txt" for i in range(self.num_workers)]
+        logging.debug("FILENAMES %s", str(file_names))
 
-		walk = [start_node]
+        args_list = []
+        for i in range(self.num_workers):
+            args_list.append((paths_per_worker[i], self.walk_length, file_names[i]))
+            
+        
+        logging.debug("Starting Pool")
 
-		while len(walk) < walk_length:
-			cur = walk[-1]
-			cur_nbrs = sorted(G.neighbors(cur))
-			if len(cur_nbrs) > 0:
-				if len(walk) == 1:
-					walk.append(cur_nbrs[self.alias_draw(alias_nodes[cur][0], alias_nodes[cur][1])])
-				else:
-					prev = walk[-2]
-					next = cur_nbrs[self.alias_draw(alias_edges[(prev, cur)][0], 
-						alias_edges[(prev, cur)][1])]
-					walk.append(next)
-			else:
-				break
+        with get_context('spawn').Pool(processes=self.num_workers) as pool:
+            res = pool.map(self.write_walks_to_disk, args_list)
 
-		return walk
+        
+        #combine_files
+        with open(self.outfile, 'w') as finalout:
+            for file_ in file_names:
+                with open(file_, 'r') as f:
+                    for line in f:
+                        finalout.write(f"{line}\n")
 
-	def simulate_walks(self, num_walks, walk_length):
-		'''
-		Repeatedly simulate random walks from each node.
-		'''
 
-		G = self.G
 
-		walks = []
-		nodes = list(G.nodes())
-		print('Walk iteration:')
-		for walk_iter in range(num_walks):
-			print(str(walk_iter+1), '/', str(num_walks))
-			random.shuffle(nodes)
-			for node in nodes:
-				walks.append(self.node2vec_walk(walk_length=walk_length, start_node=node))
+    def write_walks_to_disk(self, args):
+    
+        num_walks, walk_length, out_file = args
+        
+        with open(out_file, 'w')  as fout:
+            for walk in self.simulate_walks(num_walks, walk_length):
+                fout.write(u"{}\n".format(u" ".join(v for v in walk)))
 
-		return walks
-
-	def learn_embeddings(self, walks):
-		'''
-		Learn embeddings by optimizing the Skipgram objective using SGD.
-		'''
-		walks = [list(map(str, walk)) for walk in walks]
-		model = Word2Vec(walks, window=self.window_size, vector_size=self.dimensions, min_count=0, sg=1, workers=self.workers, epochs=self.iter)
-		model.wv.save_word2vec_format(self.embeddings_file_path)
+        return out_file
+        
+                
 	
-		return
 
-	def preprocess_transition_probs(self, G):
-		'''
-		Preprocessing of transition probabilities for guiding the random walks.
-		'''
-
-		#G = self.G
-
-		alias_nodes = {}
-		for node in G.nodes():
-			unnormalized_probs = [G[node][nbr]['weight'] for nbr in sorted(G.neighbors(node))]
-			norm_const = sum(unnormalized_probs)
-			normalized_probs =  [float(u_prob)/norm_const for u_prob in unnormalized_probs]
-			alias_nodes[node] = self.alias_setup(normalized_probs)
-
-		alias_edges = {}
-		triads = {}
-
-		if self.is_directed:
-			for edge in G.edges():
-				alias_edges[edge] = self.get_alias_edge(G, edge[0], edge[1])
-		else:
-			for edge in G.edges():
-				alias_edges[edge] = self.get_alias_edge(G, edge[0], edge[1])
-				alias_edges[(edge[1], edge[0])] = self.get_alias_edge(G, edge[1], edge[0])
-
-		self.alias_nodes = alias_nodes
-		self.alias_edges = alias_edges
-
-		return
+    def simulate_walks(self, num_walks, walk_length):
+        '''
+        Repeatedly simulate random walks from each node.
+        '''
+        nodes = self.nodes[:]
+        
+        for i in range(num_walks):
+            random.shuffle(nodes)
+            for node in nodes:
+                yield(self.node2vec_walk(walk_length=walk_length, start_node=node))
 
 
-	def alias_setup(self, probs):
-		'''
-		Compute utility lists for non-uniform sampling from discrete distributions.
-		Refer to https://hips.seas.harvard.edu/blog/2013/03/03/the-alias-method-efficient-sampling-with-many-discrete-outcomes/
-		for details
-		'''
-		K = len(probs)
-		q = np.zeros(K)
-		J = np.zeros(K, dtype=int)
+    
+    def node2vec_walk(self, walk_length, start_node):
+        '''
+        Simulate a random walk starting from start node.
+        '''
+        alias_nodes = self.alias_nodes
+        alias_edges = self.alias_edges
 
-		smaller = []
-		larger = []
-		for kk, prob in enumerate(probs):
-			q[kk] = K*prob
-			if q[kk] < 1.0:
-				smaller.append(kk)
-			else:
-				larger.append(kk)
+        walk = [start_node]
+        
+        while len(walk) < walk_length:
+            cur = walk[-1]
+            cur_nbrs = sorted(self.graph.neighbors(cur))
+            if len(cur_nbrs) > 0:
+                if len(walk) == 1:
+                    walk.append(cur_nbrs[self.alias_draw(alias_nodes[cur][0], alias_nodes[cur][1])])
+                else:
+                    prev = walk[-2]
+                    next_ = cur_nbrs[self.alias_draw(alias_edges[(prev, cur)][0], alias_edges[(prev, cur)][1])]
+                    walk.append(next_)
+            else:
+                break
 
-		while len(smaller) > 0 and len(larger) > 0:
-			small = smaller.pop()
-			large = larger.pop()
-
-			J[small] = large
-			q[large] = q[large] + q[small] - 1.0
-			if q[large] < 1.0:
-				smaller.append(large)
-			else:
-				larger.append(large)
-
-		return J, q
+        return [str(node) for node in walk]
 
 
-	def get_alias_edge(self, G, src, dst):
-		'''
-		Get the alias edge setup lists for a given edge.
-		'''
-		unnormalized_probs = []
-		for dst_nbr in sorted(G.neighbors(dst)):
-			if dst_nbr == src:
-				unnormalized_probs.append(G[dst][dst_nbr]['weight']/self.p)
-			elif G.has_edge(dst_nbr, src):
-				unnormalized_probs.append(G[dst][dst_nbr]['weight'])
-			else:
-				unnormalized_probs.append(G[dst][dst_nbr]['weight']/self.q)
-		norm_const = sum(unnormalized_probs)
-		normalized_probs =  [float(u_prob)/norm_const for u_prob in unnormalized_probs]
+    def preprocess_transition_probs(self):
+        '''
+        Preprocessing of transition probabilities for guiding the random walks.
+        '''
 
-		return self.alias_setup(normalized_probs)
+        alias_nodes = {}
+        for node in self.nodes:
+            unnormalized_probs = [self.graph[node][nbr]['weight'] for nbr in sorted(G.neighbors(node))]
+            norm_const = sum(unnormalized_probs)
+            normalized_probs =  [float(u_prob)/norm_const for u_prob in unnormalized_probs]
+            alias_nodes[node] = self.alias_setup(normalized_probs)
 
-	def alias_draw(self, J, q):
-		'''
-		Draw sample from a non-uniform discrete distribution using alias sampling.
-		'''
-		K = len(J)
+        alias_edges = {}
+        triads = {}
 
-		kk = int(np.floor(np.random.rand()*K))
-		if np.random.rand() < q[kk]:
-			return kk
-		else:
-			return J[kk]
+        if self.is_directed:
+            for edge in self.graph.edges():
+                alias_edges[edge] = self.get_alias_edge(self.graph, edge[0], edge[1])
+        else:
+            for edge in self.graph.edges():
+                alias_edges[edge] = self.get_alias_edge(self.graph, edge[0], edge[1])
+                alias_edges[(edge[1], edge[0])] = self.get_alias_edge(self.graph, edge[1], edge[0])
 
-	
-	def train(self):
-		G = self.read_graph()
-		self.preprocess_transition_probs(G)
-		walks = self.simulate_walks(self.num_walks, self.walk_length)
-		self.learn_embeddings(walks)
-		return 
-	
+        self.alias_nodes = alias_nodes
+        self.alias_edges = alias_edges
+
+        return
+
+
+    def alias_setup(self, probs):
+        '''
+        Compute utility lists for non-uniform sampling from discrete distributions.
+        Refer to https://hips.seas.harvard.edu/blog/2013/03/03/the-alias-method-efficient-sampling-with-many-discrete-outcomes/
+        for details
+        '''
+        K = len(probs)
+        q = np.zeros(K)
+        J = np.zeros(K, dtype=int)
+
+        smaller = []
+        larger = []
+        for kk, prob in enumerate(probs):
+            q[kk] = K*prob
+            if q[kk] < 1.0:
+                smaller.append(kk)
+            else:
+                larger.append(kk)
+
+        while len(smaller) > 0 and len(larger) > 0:
+            small = smaller.pop()
+            large = larger.pop()
+
+            J[small] = large
+            q[large] = q[large] + q[small] - 1.0
+            if q[large] < 1.0:
+                smaller.append(large)
+            else:
+                larger.append(large)
+
+        return J, q
+
+
+    def get_alias_edge(self, G, src, dst):
+        '''
+        Get the alias edge setup lists for a given edge.
+        '''
+        unnormalized_probs = []
+        for dst_nbr in sorted(G.neighbors(dst)):
+            if dst_nbr == src:
+                unnormalized_probs.append(G[dst][dst_nbr]['weight']/self.p)
+            elif G.has_edge(dst_nbr, src):
+                unnormalized_probs.append(G[dst][dst_nbr]['weight'])
+            else:
+                unnormalized_probs.append(G[dst][dst_nbr]['weight']/self.q)
+
+        norm_const = sum(unnormalized_probs)
+        normalized_probs =  [float(u_prob)/norm_const for u_prob in unnormalized_probs]
+        
+        return self.alias_setup(normalized_probs)
+
+    def alias_draw(self, J, q):
+        '''
+        Draw sample from a non-uniform discrete distribution using alias sampling.
+        '''
+        K = len(J)
+
+        kk = int(np.floor(np.random.rand()*K))
+        if np.random.rand() < q[kk]:
+            return kk
+        else:
+            return J[kk]
+
+
--- mowl/walking/walking.py
+++ mowl/walking/walking.py
@@ -4,13 +4,31 @@ class WalkingModel():
     '''
     :param edges: List of :class:`mowl.graph.edge.Edge`
     '''
-    def __init__(self, edges, num_paths, path_length, alpha, num_workers, outfile):    
+    def __init__(self, edges, num_walks, walk_length, num_workers, outfile):    
         self.edges = edges
-        self.num_paths = num_paths
-        self.path_length = path_length
-        self.alpha = alpha
+        self.num_walks = num_walks
+        self.walk_length = walk_length
         self.num_workers = num_workers
         self.outfile = outfile
 
+    # Abstract methods
     def walk(self):
         raise NotImplementedError()
+
+
+    # Non abstract method
+    def num_paths_per_worker(self):
+        
+        if self.num_walks <= self.num_workers:
+            paths_per_worker = [1 for x in range(self.num_walks)]
+        else:
+            remainder = self.num_walks % self.num_workers
+            aux = self.num_workers - remainder
+            paths_per_worker = [int((self.num_walks+aux)/self.num_workers) for i in range(self.num_workers)]
+            i = 0
+            while aux > 0:
+                paths_per_worker[i%self.num_workers] -= 1
+                i += 1
+                aux -= 1
+
+        return paths_per_worker
