e9628a59e034fde58386ee2ce1fd6050bb147f71
Fernando Zhapa-Camacho <ferynando7@gmail.com>
2021-12-22 01:14:58 +0300

    :construction: Updated walking factory method to include Node2Vec. Test missing

--- mowl/walking/util.py
+++ mowl/walking/util.py
@@ -2,13 +2,16 @@ from mowl.walking.deepwalk.model import DeepWalk
 
 
 
-def walking_factory(method_name, edges, num_paths, path_length, alpha, num_workers = 1, seed=0, outfile=None):
+def walking_factory(method_name, edges, num_walks, walk_length, alpha, num_workers = 1, seed=0, outfile=None, p = 1, q=1):
 
     methods = [
-        "deepwalk"
+        "deepwalk",
+        "node2vec"
     ]
 
     if method_name == "deepwalk":
-        return DeepWalk(edges, num_paths, path_length, alpha, num_workers=num_workers, seed=seed, outfile=outfile)
+        return DeepWalk(edges, num_walks, walk_length, alpha, num_workers=num_workers, seed=seed, outfile=outfile)
+    if method_name == "node2vec":
+        return Node2Vec(edges, num_walks, walk_length, p, q, num_workers=num_workers, seed=seed, outfile=outfile)
     else:
         raise Exception(f"Walking method unrecognized. Recognized methods are: {methods}")
