8f451b4764fd1ae1cff606c03bcc1e7bf7522ca5
Fernando Zhapa <ferynando7@gmail.com>
2022-01-30 14:24:05 +0300

    :sparkles: added Walking RDF and OWL in Scala.

--- mowl/walking/factory.py
+++ mowl/walking/factory.py
@@ -1,17 +1,20 @@
 from mowl.walking.deepwalk.model import DeepWalk
 from mowl.walking.node2vec.model import Node2Vec
-
+from mowl.walking.walkRdfAndOwl.model import WalkRDFAndOWL
 
 def walking_factory(method_name, edges, num_walks, walk_length, outfile, workers = 1, alpha = 0, p = 1, q=1):
 
     methods = [
         "deepwalk",
-        "node2vec"
+        "node2vec",
+        "walkrdfowl"
     ]
 
     if method_name == "deepwalk":
         return DeepWalk(edges, num_walks, walk_length, alpha, outfile, workers=workers)
-    if method_name == "node2vec":
+    elif method_name == "node2vec":
         return Node2Vec(edges, num_walks, walk_length, p, q, outfile, workers=workers)
+    elif method_name == "walkrdfowl":
+        return WalkRDFAndOWL(edges, num_walks, walk_length, outfile, workers=workers)
     else:
         raise Exception(f"Walking method unrecognized. Recognized methods are: {methods}")
