58756e5e7cbe1d892ed13ee9cb74b32092f89076
Fernando Zhapa <ferynando7@gmail.com>
2022-03-19 01:56:33 +0300

    update

--- mowl/examples/CatEmbeddings/get_splits.py
+++ mowl/examples/CatEmbeddings/get_splits.py
@@ -11,13 +11,14 @@ def get_splits(train_edges_file, closure_edges_file):
             line = line.rstrip("\n").split("\t")
             ontology_edges.append((line[0], line[1]))
 
+    entities = set([c for c,_ in ontology_edges]) | set([d for c, d in ontology_edges])
+    
     closure_edges = []
     with open(closure_edges_file, "r") as f:
         for line in f:
             line = line.rstrip("\n").split("\t")
-            closure_edges.append((line[0], line[1]))
-
-    entities = list(set([c for c,_ in ontology_edges]) | set([d for c, d in ontology_edges]))
+            if line[0] in entities and line[1] in entities:
+                closure_edges.append((line[0], line[1]))
 
     logging.info("Generating training negatives of type 1")
     train_negatives_1 = list()
@@ -25,36 +26,46 @@ def get_splits(train_edges_file, closure_edges_file):
         for c, d in bar:
             train_negatives_1.append((d,c))
 
+    logging.info("Generating closure negatives of type 1")
     closure_negatives_1 = list()
-    for c, d in closure_edges:
-        closure_negatives_1.append((d,c))
+    with ck.progressbar(closure_edges) as bar:
+        for c, d in bar:
+            closure_negatives_1.append((d,c))
 
         
     train_negatives_2 = list()
     forbidden_set = set(ontology_edges) | set(closure_edges) | set(train_negatives_1) | set(closure_negatives_1)
-    must_be_entities = entities[:]
-    for c, d in ontology_edges:
-        found = False
+    
+    list_entities = list(entities)
+    logging.info("Generating training negatives of type 2")
+    with ck.progressbar(ontology_edges) as bar:
+        
+        for c, d in bar:
+            found = False
 
-        while not found:
-            d_ = random.choice(entities)
-            if d_ in must_be_entities:
-                if not (c,d_) in forbidden_set:
-                    train_negatives_2.append((c, d_))
-                    found = True
+            while not found:
+                d_ = random.choice(list_entities)
+                if d_ in entities:
+                    if not (c,d_) in forbidden_set:
+                        train_negatives_2.append((c, d_))
+                        found = True
 
 
     closure_negatives_2 = list()
     forbidden_set |= set(train_negatives_2)
-    for c, d in closure_edges:
-        found = False
 
-        while not found:
-            d_ = random.choice(entities)
-            if d_ in must_be_entities:
-                if not (c, d_) in forbidden_set:
-                    closure_negatives_2.append((c, d_))
-                    found = True
+    logging.info("Generating closure negatives of type 2")
+    
+    with ck.progressbar(closure_edges) as bar:
+
+        for d, c in bar:
+            found = False
+            while not found:
+                d_ = random.choice(list_entities)
+                if d_ in entities:
+                    if not (c, d_) in forbidden_set:
+                        closure_negatives_2.append((c, d_))
+                        found = True
                 
     
     assert len(ontology_edges) == len(train_negatives_1)
@@ -63,15 +74,18 @@ def get_splits(train_edges_file, closure_edges_file):
 
 
     assert len(closure_edges) == len(closure_negatives_1), f"{len(closure_edges)}, {len(closure_negatives_1)}"
-    assert len(closure_edges) == len(closure_negatives_2)
+    assert len(closure_negatives_1) == len(closure_negatives_2)
     closure_set = list(zip(closure_edges, closure_negatives_1, closure_negatives_2))
-
+    random.shuffle(closure_set)
+    num_closure = len(closure_set) // 2
+    valid_set = closure_set[:num_closure]
+    test_set = closure_set[num_closure:]
 
     valid_size = len(ontology_edges) *20 //100
-    random.shuffle(closure_set)
+    
 
-    valid_set = closure_set[:valid_size]
-    test_set = closure_set[len(closure_set)-valid_size:]
+    valid_set = valid_set[:valid_size]
+    test_set = test_set[:valid_size]
     
     
     with open("data/train_data.pkl", "wb") as f:
