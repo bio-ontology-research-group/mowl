9706d27160503df9f68b3544a424da35632233d0
Fernando Zhapa <ferynando7@gmail.com>
2022-05-19 16:18:22 +0300

    :fire: removed walkRdfAndOwl as walking method as now it is equivalent to DeepWalk with edge labels

--- mowl/walking/factory.py
+++ mowl/walking/factory.py
@@ -3,7 +3,7 @@ from mowl.walking.node2vec.model import Node2Vec
 from mowl.walking.rdf2vec.model import RDF2Vec
 
 
-WALKING_METHODS = ["deepwalk", "node2vec", "walkrdfowl"]
+WALKING_METHODS = ["deepwalk", "node2vec"]
 
 
 def walking_factory(method_name, num_walks, walk_length, outfile, workers = 1, alpha = 0, p = 1, q=1):
