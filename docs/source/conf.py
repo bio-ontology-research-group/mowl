e1d839b90738e0c7d78af05bdfeabfb3edf1db58
Fernando Zhapa-Camacho <ferynando7@gmail.com>
2022-05-25 01:55:42 +0300

    :memo: mocking tdqm

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,4 +67,4 @@ source_suffix = ['.rst', '.md']
 # The master toctree document.
 master_doc = 'index'
 
-autodoc_mock_imports = ['org', 'uk', 'java', 'numpy', 'jpype', 'de', 'pandas', 'scipy', 'sklearn', 'owlready2', 'gensim', 'torch', 'rdflib', 'networkx', 'pykeen', 'node2vec', 'matplotlib']
+autodoc_mock_imports = ['org', 'uk', 'java', 'numpy', 'jpype', 'de', 'pandas', 'scipy', 'sklearn', 'owlready2', 'gensim', 'torch', 'rdflib', 'networkx', 'pykeen', 'node2vec', 'matplotlib', 'tqdm']
