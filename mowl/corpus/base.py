3bbc7fe4bc54433ced17235ca3edb37b1d7cb20f
Fernando Zhapa <ferynando7@gmail.com>
2022-06-26 15:51:28 +0300

    :art: update script to allow returning or saving corpus

--- mowl/corpus/base.py
+++ mowl/corpus/base.py
@@ -10,20 +10,37 @@ import logging
 
 
 
-def extract_axiom_corpus(ontology, out_file):
+def extract_and_save_axiom_corpus(ontology, out_file):
+        
+    logging.info("Generating axioms corpus")
+    renderer = ManchesterOWLSyntaxOWLObjectRendererImpl()
+    shortFormProvider = MOWLShortFormProvider()
+    renderer.setShortFormProvider(shortFormProvider)
+    with open(out_file, 'w') as f:
+        for owl_class in ontology.getClassesInSignature():
+            axioms = ontology.getAxioms(owl_class)
+            for axiom in axioms:
+                rax = renderer.render(axiom)
+                rax = rax.replaceAll(JString("[\\r\\n|\\r|\\n()|<|>]"), JString(""))
+                f.write(f'{rax}\n')
 
-        logging.info("Generating axioms corpus")
-        renderer = ManchesterOWLSyntaxOWLObjectRendererImpl()
-        shortFormProvider = MOWLShortFormProvider()
-        renderer.setShortFormProvider(shortFormProvider)
-        with open(out_file, 'w') as f:
-            for owl_class in ontology.getClassesInSignature():
-                axioms = ontology.getAxioms(owl_class)
-                for axiom in axioms:
-                    rax = renderer.render(axiom)
-                    rax = rax.replaceAll(JString("[\\r\\n|\\r|\\n()|<|>]"), JString(""))
-                    f.write(f'{rax}\n')
+def extract_axiom_corpus(ontology):
+
+    logging.info("Generating axioms corpus")
+    renderer = ManchesterOWLSyntaxOWLObjectRendererImpl()
+    shortFormProvider = MOWLShortFormProvider()
+    renderer.setShortFormProvider(shortFormProvider)
+
+    corpus = []
     
+    for owl_class in ontology.getClassesInSignature():
+        axioms = ontology.getAxioms(owl_class)
+        for axiom in axioms:
+            rax = renderer.render(axiom)
+            rax = rax.replaceAll(JString("[\\r\\n|\\r|\\n()|<|>]"), JString(""))
+            corpus.append(rax)
+    return corpus
+
 
 
 def extract_annotation_corpus(ontology, out_file, mode = "append"):
