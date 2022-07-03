4e94f149327fda65c0d18ad20eaa8b64b3018ea5
Fernando Zhapa-Camacho <ferynando7@gmail.com>
2022-07-03 23:32:48 +0300

    :memo: :fire: removed deprecated files and updated docs

--- mowl/walking/factory.py
+++ mowl/walking/factory.py
@@ -1,7 +1,5 @@
 from mowl.walking.deepwalk.model import DeepWalk
 from mowl.walking.node2vec.model import Node2Vec
-from mowl.walking.rdf2vec.model import RDF2Vec
-
 
 WALKING_METHODS = ["deepwalk", "node2vec"]
 
@@ -12,7 +10,5 @@ def walking_factory(method_name, num_walks, walk_length, outfile = None, workers
         return DeepWalk(num_walks, walk_length, alpha, outfile, workers=workers)
     elif method_name == "node2vec":
         return Node2Vec(num_walks, walk_length, p, q, outfile, workers=workers)
-    elif method_name == "walkrdfowl":
-        return RDF2Vec([], num_walks, walk_length, outfile, workers=workers)
     else:
         raise Exception(f"Walking method unrecognized. Recognized methods are: {WALKING_METHODS}")
