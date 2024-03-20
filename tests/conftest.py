e8b57c8f577f7692bf9e57059aecbf695aad49ea
Fernando Zhapa-Camacho <ferzcam@gmail.com>
2024-03-20 10:44:57 +0300

    :bug: remove test tearDownModule method

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,15 +22,15 @@ def pytest_configure(config):
     PPIYeastSlimDataset()
 
 
-def pytest_unconfigure(config):
-    os.remove('ppi_yeast_slim.tar.gz')
-    os.remove('gda_human_el.tar.gz')
-    os.remove('gda_mouse_el.tar.gz')
-    os.remove('family.tar.gz')
-    shutil.rmtree('ppi_yeast_slim')
-    shutil.rmtree('gda_human_el')
-    shutil.rmtree('gda_mouse_el')
-    shutil.rmtree('family')
+# def pytest_unconfigure(config):
+    # os.remove('ppi_yeast_slim.tar.gz')
+    # os.remove('gda_human_el.tar.gz')
+    # os.remove('gda_mouse_el.tar.gz')
+    # os.remove('family.tar.gz')
+    # shutil.rmtree('ppi_yeast_slim')
+    # shutil.rmtree('gda_human_el')
+    # shutil.rmtree('gda_mouse_el')
+    # shutil.rmtree('family')
 
 
 
