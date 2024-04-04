098e47ca58d5fd4de1587c35a326235ee3471ee4
Fernando Zhapa-Camacho <ferzcam@gmail.com>
2024-04-04 14:33:29 +0300

    :sparkles: Renamed ELBoxModule to ELBEModule

--- mowl/nn/__init__.py
+++ mowl/nn/__init__.py
@@ -1,4 +1,4 @@
 from .el.elmodule import ELModule
 from .el.elem.module import ELEmModule
-from .el.elbe.module import ELBoxModule
+from .el.elbe.module import ELBoxModule, ELBEModule
 from .el.boxsquaredel.module import BoxSquaredELModule
