8ad11215a35934b6b0a7c594a134eeb930987812
Fernando Zhapa <ferzcam@gmail.com>
2025-10-05 18:26:01 +0300

    :bug: download datasets for unittests

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,3 +9,24 @@ from mowl.datasets.builtin import FamilyDataset, GDAHumanELDataset, GDAMouseELDa
 
 logger = logging.getLogger("Downloader")
 
+
+def setUpModule():
+    print("Downloading datasets")
+    logger.info("Downloading family dataset")
+    FamilyDataset()
+    logger.info("Downloading gda_el_human dataset")
+    GDAHumanELDataset()
+    logger.info("Downloading gda_el_mouse dataset")
+    GDAMouseELDataset()
+    logger.info("Downloading ppi_yeast_slim dataset")
+    PPIYeastSlimDataset()
+    logger.info("Downloading go_subsumption dataset")
+    GOSubsumptionDataset()
+    logger.info("Downloading foodon_subsumption dataset")
+    FoodOnSubsumptionDataset()
+    logger.info("Downloading gda_v2 dataset")
+    GDADatasetV2()
+    logger.info("Downloading ppi_human dataset")
+    PPIHumanDataset()
+    logger.info("Downloading hpi dataset")
+    HPIDataset()
